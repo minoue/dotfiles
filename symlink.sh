288b540edaf016474f67be89222986185a34cbe3
minoue <michitaka.inoue@icloud.com>
2019-10-09 08:18:13 +0100

    Update symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$(uname)" == 'Darwin' ]; then
+    OS='Mac'
+    ln -sf ~/dotfiles/vim/vimrc.vim ~/.vimrc
+    ln -sf ~/dotfiles/vim/gvimrc.vim ~/.gvimrc
+    ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
+    OS='Cygwin'
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
+echo "dotfiles has been updated"
