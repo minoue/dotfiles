294686f181af75bb7f6af92eedcc7c62ff6c5e0a
minoue <minoue@localhost.localdomain>
2020-05-15 23:10:50 +0100

    Update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -7,6 +7,10 @@ if [ "$(uname)" == 'Darwin' ]; then
     ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
+    ln -sf ~/dotfiles/vim/vimrc.vim ~/.vimrc
+    ln -sf ~/dotfiles/vim/gvimrc.vim ~/.gvimrc
+    ln -sf ~/dotfiles/vim/linux.vim ~/linux.vim
+    ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
     OS='Cygwin'
 else
