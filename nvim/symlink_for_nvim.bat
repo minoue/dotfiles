baf9758ca64d5fa7c9cc889bf85665340c2c918f
minoue <michitaka.inoue@icloud.com>
2024-05-08 08:21:14 +0100

    update install script

--- nvim/symlink_for_nvim.bat
+++ nvim/symlink_for_nvim.bat
@@ -1,6 +1,7 @@
 cd /d %~dp0
 
 mklink C:%HOMEPATH%\AppData\Local\nvim\init.vim %CD%\init.vim
+mklink C:%HOMEPATH%\AppData\Local\nvim\ginit.vim %CD%\ginit.vim
 mklink C:%HOMEPATH%\AppData\Local\nvim\win.vim %CD%\win.vim
 
 pause
